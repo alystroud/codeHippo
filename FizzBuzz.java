2b8cb1cb6571c15724f5d0a8dc9f73c3cfe1647d
alystroud <alygrace@hotmail.co.uk>
2017-07-29 11:21:48 +0100

    FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,26 +1,20 @@
 public class FizzBuzz{
-  void run(){
+  public static void main(String[] args){
     for(int i = 1; i <= 100; i++){
-    if(i % 3 == 0){
-      if(i % 5 == 0){
-        System.out.println("FizzBuzz");
+      if(i % 3 == 0){
+        if(i % 5 == 0){
+          System.out.println("FizzBuzz");
+        }
+        else{
+          System.out.println("Fizz");
+        }
+      }
+      else if(i % 5 == 0){
+        System.out.println("Buzz");
       }
       else{
-        System.out.println("Fizz");
+        System.out.println(i);
       }
     }
-    else if(i % 5 == 0){
-      System.out.println("Buzz");
-    }
-    else{
-      System.out.println(i);
-    }
-  }
-}
-
-  public static void main(String[] args){
-    FizzBuzz fizzbuzz = new FizzBuzz();
-    fizzbuzz.run();
-    return;
   }
 }
