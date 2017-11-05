d1395fdd04f97dd21e6e1ecc5ce993faf7b50e5e
alystroud <alygrace@hotmail.co.uk>
2017-11-05 10:16:53 +0000

    Variables Post

---
+++ Variables.java
@@ -0,0 +1,11 @@
+class Variables{
+
+  public static void main(String[] args){
+    String hippoName = "Hetty";
+    int hippoAge = 5;
+
+    //Hetty has a birthday so is now one year older
+    hippoAge = hippoAge + 1;
+    System.out.println(hippoName + " the hippo is " + hippoAge + " years old!");
+  }
+}
